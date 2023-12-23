4602f8d3a29b05492ac3510109789c570c739d79
hyenicil <57847728+hyenicil@users.noreply.github.com>
2023-12-24 01:03:21 +0300

    Update StonePaperScissors.java
    Kontrol et

--- src/stone/paper/scissors/StonePaperScissors.java
+++ src/stone/paper/scissors/StonePaperScissors.java
@@ -1,19 +1,33 @@
-package stone.paper.scissors;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class StonePaperScissors {
+    public static void main(String[] args) {
+
+        mainMenu();
+    }
 
     private static ArrayList<String> scores = new ArrayList<>();
 
+    private static void scores () {
+
+        if (scores.isEmpty()) {
+            System.out.println("There is no score.");
+        } else {
+            System.out.println("Scores:");
+            Collections.sort(scores);
+
+            for (String score : scores) {
+                System.out.println(score);
+            }
+        }
+        mainMenu();
+    }
     public static void mainMenu() {
         Scanner input = new Scanner(System.in);
-        System.out.println("1->New Game");
-        System.out.println("2->Scores");
-        System.out.println("3->Exit");
-        System.out.println("Choose:");
+        printFirstMenus();;
         int begin = input.nextInt();
         switch (begin) {
             case 1:
@@ -32,19 +46,41 @@ public class StonePaperScissors {
         }
 
     }
+    public static void printFirstMenus() {
+        System.out.println("1->New Game");
+        System.out.println("2->Scores");
+        System.out.println("3->Exit");
+        System.out.println("Choose:");
+    }
+    public static void printSecondMenus() {
+        System.out.println("Welcome to rock, paper, scissors game.");
+        System.out.println("1->STONE");
+        System.out.println("2->SCISSORS");
+        System.out.println("3->PAPER");
+        System.out.println("4->Main Menu");
+        System.out.println("Please chose a number:");
+    }
 
+    public static String glopalPrint(int a) {
+       return  switch (a){
+            case 1 ->"STONE";
+            case 2 ->"SCISSORS";
+            default -> "PAPER";
+        };
+    }
+
+
+    public static void printStatus(int choosen, int pc,String message)
+    {
+        System.out.println("You chose->"+ glopalPrint(choosen) + " Pc chose->"+ glopalPrint(pc));
+        System.out.println(message);
+    }
     public static void newGame() {
         Scanner input = new Scanner(System.in);
         int a = 0;
         int score = 0;
         while (a == 0) {
-            System.out.println("Welcome to rock, paper, scissors game.");
-
-            System.out.println("1->STONE");
-            System.out.println("2->SCISSORS");
-            System.out.println("3->PAPER");
-            System.out.println("4->Main Menu");
-            System.out.println("Please chose a number:");
+            printSecondMenus();
             int choosen = input.nextInt();
             int pc = (int) (1 + Math.random() * 3);
             if (choosen == 4) {
@@ -52,75 +88,44 @@ public class StonePaperScissors {
             }
             if (pc == choosen) {
                 if (choosen == 1) {
-                    System.out.println("You chose->Stone" + "  Pc chose->Stone");
-                    System.out.println("Draw.");
+                    printStatus(choosen, pc, "Draw.");
                 } else if (choosen == 2) {
-                    System.out.println("You chose->Scissors" + "  Pc chose->Scissors");
-                    System.out.println("Draw.");
+                    printStatus(choosen, pc, "Draw.");
                 } else if (choosen == 3) {
-                    System.out.println("You chose->Paper" + "  Pc chose->Paper");
-                    System.out.println("Draw.");
+                    printStatus(choosen, pc, "Draw.");
                 }
 
-            } else if (choosen == 1 && pc == 2) {
-                System.out.println("You chose->Stone" + "  Pc chose->Scissors");
-                System.out.println("You won.");
-                score += 10;
-                System.out.println("You win.\nYour point is " + score);
-                scores.add("Point: " + score);
-            } else if (choosen == 1 && pc == 3) {
-                System.out.println("You chose->Stone" + "  Pc chose->Paper");
-                System.out.println("You lost.");
-                score -= 10;
-                System.out.println("You win.\nYour point is " + score);
-                scores.add("Point: " + score);
-            } else if (choosen == 2 && pc == 1) {
-                System.out.println("You chose->Scissors" + "  Pc chose->Stone");
-                System.out.println("You lost.");
-                score -= 10;
-                System.out.println("You win.\nYour point is " + score);
-                scores.add("Point: " + score);
-            } else if (choosen == 2 && pc == 3) {
-                System.out.println("You chose->Scissors" + "  Pc chose->Paper");
-                System.out.println("You won.");
-                score += 10;
-                System.out.println("You win.\nYour point is " + score);
-                scores.add("Point: " + score);
-            } else if (choosen == 3 && pc == 1) {
-                System.out.println("You chose->Paper" + "  Pc chose->Stone");
-                System.out.println("You won.");
-                score += 10;
-                System.out.println("You win.\nYour point is " + score);
-                scores.add("Point: " + score);
-            } else if (choosen == 3 && pc == 2) {
-                System.out.println("You chose->Paper" + "  Pc chose->Scissors");
-                System.out.println("You lost.");
-                score -= 10;
-                System.out.println("You win.\nYour point is " + score);
-                scores.add("Point: " + score);
+            }else if (choosen == 1 && pc == 2) {
+                    printStatus(choosen, pc, "You won.");
+                    score += 10;
+                    System.out.println("You win.\nYour point is " + score);
+                    scores.add("Point: " + score);
+                } else if (choosen == 1 && pc == 3) {
+                    printStatus(choosen, pc, "You lost.");
+                    score -= 10;
+                    System.out.println("You win.\nYour point is " + score);
+                    scores.add("Point: " + score);
+                } else if (choosen == 2 && pc == 1) {
+                    printStatus(choosen, pc, "You lost.");
+                    score -= 10;
+                    System.out.println("You win.\nYour point is " + score);
+                    scores.add("Point: " + score);
+                } else if (choosen == 2 && pc == 3) {
+                    printStatus(choosen, pc, "You won.");
+                    score += 10;
+                    System.out.println("You win.\nYour point is " + score);
+                    scores.add("Point: " + score);
+                } else if (choosen == 3 && pc == 1) {
+                    printStatus(choosen, pc, "You won.");
+                    score += 10;
+                    System.out.println("You win.\nYour point is " + score);
+                    scores.add("Point: " + score);
+                } else if (choosen == 3 && pc == 2) {
+                    printStatus(choosen, pc, "You won.");
+                    score -= 10;
+                    System.out.println("You win.\nYour point is " + score);
+                    scores.add("Point: " + score);
+                }
             }
         }
-
     }
-
-    private static void scores() {
-
-        if (scores.isEmpty()) {
-            System.out.println("There is no score.");
-        } else {
-            System.out.println("Scores:");
-            Collections.sort(scores);
-
-            for (String score : scores) {
-                System.out.println(score);
-            }
-        }
-        mainMenu();
-    }
-
-    public static void main(String[] args) {
-
-        mainMenu();
-    }
-
-}
